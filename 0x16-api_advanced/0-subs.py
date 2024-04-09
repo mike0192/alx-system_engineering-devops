0e658fc1359e996c18f1afb260dc549ae6dea3f9
michael <michaelhagos163@gmail.com>
2024-04-09 09:28:50 -0700

    number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -18,18 +18,11 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    If not a valid subreddit, return 0.
-    """
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    user_agent = 'User Agent'
-    headers = {
-        'User-Agent': user_agent
-    }
-
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        result = response.json().get('data')
-        return result.get('subscribers')
-    else:
+    data = response.json()
+    if response.status_code != 200:
         return 0
+    else:
+        return data['data']['subscribers']
