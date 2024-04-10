82bb3de36019c794603d5ff2d4e7337952043c12
michael <michaelhagos163@gmail.com>
2024-04-10 00:52:00 -0700

    returns the number of subs and query reddit api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,9 +8,9 @@ import requests
 def number_of_subscribers(subreddit):
     """return the total number of subscribers"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
+    headers = {"User-Agent": "mike"}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
+    if response.status_code == 400:
         return 0
-    data = response.json().get("data")
-    return data.get("subscribers")
+    R = response.json().get("data")
+    return R.get("subscribers")
