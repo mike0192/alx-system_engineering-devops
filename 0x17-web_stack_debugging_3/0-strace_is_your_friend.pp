cea03b3fc1c50df91da80b64aa1dee5bc2264a31
michael <michaelhagos163@gmail.com>
2024-04-11 19:18:19 +0000

    test

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # puppet code to fix WordPress webstack bug
-exec { 'debugging fix':
-  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
-  path    => '/bin'
+exec {'fix wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
   }
